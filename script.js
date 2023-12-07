d975f0c2c096becaece6b25baf1c07f00918206d
camilleandrade125 <camilleandrade125@gmail.com>
2023-12-07 12:21:02 -0300

    Adicionando estrutura lógica e estilo ao jogo.

--- script.js
+++ script.js
@@ -1,22 +1,55 @@
 const campoMinado = [];
+let pontuacao = 0;
 
-function faseAleatoria (){
+function faseAleatoria (h, we){
 
-    for (let i = 0; i < 5; i++)
+    const campo = document.getElementById("campo-minado")
+
+    for (let i = 0; i < h; i++)
     {
         campoMinado.push([]);
+        const div = document.createElement("div")
+        campo.appendChild(div)
+        div.className = "fundacao"
+
+
     
-        for (let w = 0; w < 5; w++)
+        for (let w = 0; w < we; w++)
         {
+            const divHorizontal = document.createElement("div")
+            divHorizontal.className = "bloco-horizontal"
+            div.appendChild(divHorizontal)
             const numberAleatorio = Math.random() * 2
             const numerointeiro = Math.ceil(numberAleatorio)
             campoMinado[i].push(numerointeiro);
+            divHorizontal.setAttribute("data-info", numerointeiro)
+
+
         }
-    
          
     }
-console.log(campoMinado);
 
 }
 
-faseAleatoria() 
+faseAleatoria(5, 5) 
+
+const blocos = document.getElementsByClassName("bloco-horizontal")
+const modal = document.getElementById("gamerOver")
+
+const novosBlocos = [...blocos]
+novosBlocos.forEach(element => {
+    element.addEventListener("click", function(){
+       const bombaOuNao = element.getAttribute("data-info")
+       if (bombaOuNao == 1) {
+        element.style.backgroundImage = "url(./bomba.png)"
+        modal.style.display = "flex";
+    
+       }
+    
+    })
+});
+
+function aoclicar(){
+    window.location.reload();
+
+}
